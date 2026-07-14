40a338f28d84238bef5adeb215e8c3e46e2d91b2
WedgemikWoon <woonhaochan@gmail.com>
2026-07-14 21:21:17 +0800

    Add Express server to collect IP addresses

--- Wedge.js
+++ Wedge.js
@@ -1,1 +1,17 @@
-// hi
+const express = require("express");
+
+const app = express();
+
+app.post("/collect-ip", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  console.log("Consent received. IP:", ip);
+
+  res.json({
+    success: true
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
